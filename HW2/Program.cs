acdc69e26b53bebac252c901801b03a42cc7ee07
Natalia Landik <landik.96@mail.ru>
2022-11-22 22:26:39 +0300

    Добавила гитигнор

---
+++ HW2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве//
+
+
+int aX = ReadInt("Введите координату Х точки А: ");
+int aY = ReadInt("Введите координату У точки А: ");
+int aZ = ReadInt("Введите координату Z точки А: ");
+int bX = ReadInt("Введите координату Х точки B: ");
+int bY = ReadInt("Введите координату У точки B: ");
+int bZ = ReadInt("Введите координату Z точки B: ");
+
+int sqrSideX = (aX-bX) * (aX- bX);
+int sqrSideY = (aY-bY) * (aY- bY);
+int sqrSideZ = (aZ-bZ) * (aZ- bZ);
+Double distance = Math.Sqrt(sqrSideX+sqrSideY + sqrSideZ);
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
---
+++ HW3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N//
+
+int number = ReadInt("Введите число: ");
+int i = 1;
+while(i <= number)
+{
+    Console.Write(Pow(i++) + " ");
+}
+
+int Pow(int a)
+{
+    return a * a * a;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}

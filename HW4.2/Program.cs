f14f2f01b3f215cd001b42c92cd6aeae23d70469
Natalia Landik <landik.96@mail.ru>
2022-11-25 13:21:48 +0300

    сделала дз 4 семинара

---
+++ HW4.2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B//
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = Convert.ToInt32(Math.Pow(numberA, numberB));
+
+Console.WriteLine($"Число {numberA} в натуральной степени {numberB} равно {result}");
---
+++ HW4.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран. Вывод сделать отдельным методом//
+
+Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[size];
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next();
+    Console.Write(numbers[i] + " ");
+}
+
+// не совсем поняла, что значит сделать вывод отдельным методом

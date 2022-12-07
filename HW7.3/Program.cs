af81165a49ad504338b1be0a31a8639dcdc3f822
Natalia Landik <landik.96@mail.ru>
2022-12-07 20:39:21 +0300

    ДЗ к семинару 7

---
+++ HW7.3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+int[] summ = new int[columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+int firstIndex = 0;
+int secondIndex = numbers.GetLength(0) - 1;
+
+for(int i = 0; i < numbers.GetLength(1); i++)
+{
+    Console.Write(numbers[rows, columns]);
+    summ[i] += numbers[rows, columns];
+}
+WriteMatrix(numbers);
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+
+void WriteMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+            foreach (double elem in summ)
+            {
+                
+                Console.WriteLine(elem/columns);
+            }
+
+//Не дорешала. Я очень усталь. Пусть останется так. Сорри)))
